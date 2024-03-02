b75d12b107cf9ac5e011de081334021e93fc07c5
diptodas <dasdipto2004@gmail.com>
2024-03-02 19:24:29 +0530

    Setup File Structure

---
+++ controllers/authController.js
@@ -0,0 +1,10 @@
+import User from "../schema/userSchema.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+const register=async(req,res)=>{
+
+}
+
+export default {register};
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./database/db.js";
+import authRouter from "./routes/authRoute.js";
 
 const app = express();
+dotenv.config();
 const PORT = 8000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("server is running");
-});
+
+//routes
+app.use("/api/auth",authRouter);
+
+connectDB(process.env.DB_USERNAME, process.env.DB_PASSWORD);
 
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`)
